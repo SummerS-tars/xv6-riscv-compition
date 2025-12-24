87a8c772f6b348f70debfc69e552faff378883be
thesumst <2240765672@qq.com>
2025-12-24 17:13:20 +0000

    feat: 在用户头文件中添加 mmap 和 munmap 函数声明

--- user/user.h
+++ user/user.h
@@ -24,6 +24,8 @@ int getpid(void);
 char* sys_sbrk(int,int);
 int pause(int);
 int uptime(void);
+void* mmap(void*, int, int, int, int, int);
+int munmap(void*, int);
 
 // ulib.c
 int stat(const char*, struct stat*);
